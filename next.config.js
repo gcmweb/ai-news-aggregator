5b5a1f1028a826ed2c2d903d1dc82577c8863be8
gcmweb <gcmweb2023@outlook.com>
2025-06-20 07:02:10 +0100

    chore: fix prisma deployment for Netlify - remove problematic postinstall script, add correct binary targets, update build process

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
